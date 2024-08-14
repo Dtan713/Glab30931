bd9f243eafcd422e20eee4ceea8da0a52daeec53
Dtan713 <s.donaldtan@gmail.com>
2024-08-14 08:56:22 -0400

    part 1&2

---
+++ thymeleaf-tour/src/main/java/MyController/HomeController.java
@@ -0,0 +1,21 @@
+package MyController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+
+public class HomeController{
+
+    @GetMapping("/show flowerList")
+    public String sendDataToHtml(Model model) {
+        String[] flowers = new String[] { "Rose", "Lily", "Tulip", "Carnation", "Hyacinth" };
+        String[] City = new String[] { "nyc", "nj", "dallas", "chicago", "philadelphia" };
+        model.addAttribute("flowersVariable", flowers);
+        model.addAttribute("CityVariable", City);
+        return "ViewFlowers";
+    }
+}
+
